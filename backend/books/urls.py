34440d1fd0a6d21629ce76e4da163c628ff6f324
Yurchik-Os <yura.osiychuk@gmail.com>
2025-05-29 14:10:18 +0300

    123
    12321212

--- backend/books/urls.py
+++ backend/books/urls.py
@@ -1,5 +1,14 @@
+
 from django.urls import path
 from .views import BookListCreateView, BookUpdateDeleteView
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+router = DefaultRouter()
+router.register('books', BookViewSet, basename='book')
+
 
 urlpatterns = [
     path('books/', BookListCreateView.as_view(), name='book-list-create'),
