83df32d8e4a99fc6d8500924af5c2254d7749bf0
Dmytro6914 <dmytro.02272@gmail.com>
2025-05-30 14:40:22 +0300

    перевіряйте
    перевіряйте

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
   if (adminButton && userRole === "admin") {
     adminButton.style.display = "inline-block";
     adminButton.addEventListener("click", () => {
-      window.location.href = "pages/admin_panel.html";
+      window.location.href = "admin_panel.html";
     });
   }
 });
